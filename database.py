d3fb92b1bd7ccb38f3bc1c5bde98f587996675a3
Tej Poudel <poudelp891@gmail.com>
2026-09-22 09:39:58 +0545

    Move database configuration to environment variables

--- database.py
+++ database.py
@@ -1,7 +1,12 @@
+import os
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "postgresql+psycopg://postgres:postgres@localhost:5432/expense_tracker"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
